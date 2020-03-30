ce944799deb8b95db30778de27fa6d28d8d10c9f
Miron Banks <madapegarage@gmail.com>
2020-03-30 15:40:38 +0200

    cancelling combat with movement

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -11,8 +11,9 @@ namespace RPG.Combat
         Transform target;
         private void Update()
         {
-            bool isInRange = Vector3.Distance(transform.position, target.position) < weaponRange;
-            if (target != null && isInRange)
+            if (target == null) return;
+
+            if (!GetIsInRange())
             {
                 GetComponent<Mover>().MoveTo(target.position);
             }
@@ -21,9 +22,20 @@ namespace RPG.Combat
                 GetComponent<Mover>().Stop();
             }
         }
+
+        private bool GetIsInRange()
+        {
+            return Vector3.Distance(transform.position, target.position) < weaponRange;
+        }
+
         public void Attack(CombatTarget combatTarget)
         {
             target = combatTarget.transform;
+        }
+
+        public void Cancel()
+        {
+            target = null;
         }
     }
 }
