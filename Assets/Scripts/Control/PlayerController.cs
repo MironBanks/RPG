5967af77e6497bdcc5da9e388bcbe4b9a97d1097
Miron Banks <madapegarage@gmail.com>
2020-03-30 15:09:16 +0200

    move within range

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -13,7 +13,7 @@ namespace RPG.Control
         {
             if (InteractWithCombat()) return;
             if (InteractWithMovement()) return;
-            print("Nothing to do");
+
         }
 
         private bool InteractWithCombat()
